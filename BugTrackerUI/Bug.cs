6f56ce661ff30a33257bcbb68b0693d9e42fd98d
mfclemenson <41173519+mfclemenson@users.noreply.github.com>
2021-08-09 16:35:13 -0400

    section 5

--- BugTrackerUI/Bug.cs
+++ BugTrackerUI/Bug.cs
@@ -9,11 +9,13 @@ namespace BugTrackerUI
     public class Bug
     {
         public int Id { get; set; }
-
+        [Required]
         public string Title { get; set; }
-
+        [MinLength(10)]
+        [Required]
         public string Description { get; set; }
-
+        [Range(1,5)]
+        [Required]
         public int Priority { get; set; }
     }
 }
